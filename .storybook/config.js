3303941e4c25f24a3d2e1e95dd92363a1885e85a
lubos-conf <47181387+lubos-conf@users.noreply.github.com>
2019-02-07 18:34:19 +0000

    added viewport storybook addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { configureViewport, INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { configure, addDecorator } from '@storybook/react';
 import './defaultStorybook.css';
 
-addDecorator(story => <div className='tealContentBox'><div className='middle'>{story()}</div></div>);
+const newViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px'
+    }
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px'
+    }
+  }
+};
+
+configureViewport({
+  viewports: {
+    ...INITIAL_VIEWPORTS,
+    ...newViewports
+  }
+});
+
+addDecorator(story => <div className='middle'>{story()}</div>);
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
